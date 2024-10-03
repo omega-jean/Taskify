fd262911c61adf6761b3e59a2fc37224d64c1b1a
omega-jean <jeanalex1405@gmail.com>
2024-10-03 15:02:51 +0200

    add Home page and Login page

---
+++ Angular/taskify-frontend/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    RouterOutlet
+],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
