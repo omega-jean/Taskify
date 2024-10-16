8c021ba29ff1cee1c4f2066ccea9ecd52bb13ec1
omega-jean <jeanalex1405@gmail.com>
2024-10-16 15:39:57 +0200

    add app files

--- Angular/taskify-frontend/src/app/app.component.ts
+++ Angular/taskify-frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
-    RouterOutlet
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    ReactiveFormsModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- Angular/taskify-frontend/src/app/app.routes.ts
+++ Angular/taskify-frontend/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ModifyPasswordComponent } from './modify-password/modify-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
+import { YourBoardComponent } from './your-board/your-board.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'modify-password', component: ModifyPasswordComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent},
+  { path: 'task-manager/:name', component: TaskManagerComponent},
+  { path: 'your-board', component: YourBoardComponent}
 ];
