c24a96c60edec5878d35dc0c2bac90e6f1112304
omega-jean <jeanalex1405@gmail.com>
2024-10-16 10:52:57 +0200

    add profile files

---
+++ Angular/taskify-frontend/src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+}
