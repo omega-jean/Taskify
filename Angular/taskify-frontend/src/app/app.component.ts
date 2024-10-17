b398be05af09941829eece015753149ef410899b
omega-jean <jeanalex1405@gmail.com>
2024-10-17 14:59:22 +0200

    add files modify

--- Angular/taskify-frontend/src/app/app.component.ts
+++ Angular/taskify-frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
