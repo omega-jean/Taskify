13d2ba726b8a4788fa654056d0c55de183641f1d
omega-jean <jeanalex1405@gmail.com>
2024-10-22 14:22:47 +0200

    add file for login in ma site

--- Angular/taskify-frontend/src/app/login/login.component.ts
+++ Angular/taskify-frontend/src/app/login/login.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
   imports: [
+    FormsModule,
     RouterLink,
     RouterLinkActive
-  ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  ]
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  errorMessage: string | null = null;
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
     console.log('Email:', this.email);
     console.log('Password:', this.password);
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        this.authService.saveToken(response.token);
+        this.router.navigate(['/your-board']);
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = err.error.error || 'An error occurred while logging in.';
+      }
+    });
   }
 }
