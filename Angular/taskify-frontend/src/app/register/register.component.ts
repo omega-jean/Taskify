4844a1928f3ef925555725b3f9fa45c75626229c
omega-jean <jeanalex1405@gmail.com>
2024-10-22 14:23:24 +0200

    add file for register in my site

--- Angular/taskify-frontend/src/app/register/register.component.ts
+++ Angular/taskify-frontend/src/app/register/register.component.ts
@@ -1,16 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    RouterLink,
-    RouterLinkActive
-  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule
+  ],
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  errorMessage: string | null = null;
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { email, password, confirmPassword } = this.registerForm.value;
+
+      this.http.post('http://127.0.0.1:5000/api/register', { email, password, confirmPassword }).subscribe({
+        next: (response) => {
+          console.log(response);
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          this.errorMessage = err.error ? err.error.error : 'An error has occurred. Please try again.';
+        }
+      });
+    }
+    return false;
+  }
 }
